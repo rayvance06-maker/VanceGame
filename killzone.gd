fc8382ac25a8ad296ade784bbd9c8811a4dd0ac1
Ra Vance Gonzaga <rayvance06@gmail.com>
2026-08-26 13:53:05 +0800

    Add files via upload

--- killzone.gd
+++ killzone.gd
@@ -2,8 +2,15 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_body_entered(_body: Node2D) -> void:
-	print("You Died!")
-	timer.start()
-	
-	
+func _on_body_entered(body: Node2D) -> void:
+	# Make sure only the Player triggers death
+	if body.is_in_group("player") or body.name == "Player":
+		print("You died!")
+
+		body.queue_free()
+		
+		timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0 # Reset time scale if frozen
+	get_tree().reload_current_scene()
